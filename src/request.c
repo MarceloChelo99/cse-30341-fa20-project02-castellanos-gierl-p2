0a15100503d26c87b824e05d938d8ea796a5cad7
itspaulyg <pgierl@nd.edu>
2020-09-21 16:44:07 -0400

    request success

--- src/request.c
+++ src/request.c
@@ -15,9 +15,9 @@
 Request * request_create(const char *method, const char *uri, const char *body) {
     Request *r = calloc(1, sizeof(Request));
     if (r) {
-	    r->method   = (char *) method;
-	    r->uri      = (char *) uri;
-	    r->body     = (char *) body;
+	    r->method   = strdup(method);
+	    r->uri      = strdup(uri);
+	    r->body     = strdup(body);
 	}
     return r;
 }
@@ -27,6 +27,9 @@ Request * request_create(const char *method, const char *uri, const char *body) 
  * @param   r           Request structure.
  */
 void request_delete(Request *r) {
+	free(r->method);
+	free(r->uri);
+	free(r->body);
     free(r->next);
     free(r);
 
@@ -44,11 +47,8 @@ void request_delete(Request *r) {
  * @param   fs          Socket file stream.
  */
 void request_write(Request *r, FILE *fs) {
-   fprintf(fs, "%s %s HTTP/1.0\r\n\
-                Content-Length: %lu\r\n\
-                \r\n\
-                %s",
-                r->method, r->uri, strlen(r->body), r->body); 
+   fprintf(fs, "%s %s HTTP/1.0\r\nContent-Length: %zu\r\n\r\n%s", 
+			r->method, r->uri, strlen(r->body), r->body); 
 }
 
 /* vim: set expandtab sts=4 sw=4 ts=8 ft=c: */ 
